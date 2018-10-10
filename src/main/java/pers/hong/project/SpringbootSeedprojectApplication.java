67ed09e943f6d8a2191d08776849b2759bf26138
hong52ni <805258462@qq.com>
2018-10-10 11:31:22 +0800

    m

---
+++ src/main/java/pers/hong/project/SpringbootSeedprojectApplication.java
@@ -0,0 +1,12 @@
+package pers.hong.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSeedprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSeedprojectApplication.class, args);
+    }
+}
