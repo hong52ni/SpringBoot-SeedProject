9682755e0f23ea586375f0e20e0dc186a57b3dd3
zwh <805258462@qq.com>
2018-10-18 20:59:20 +0800

    常量类

---
+++ src/main/java/pers/hong/project/common/Constants.java
@@ -0,0 +1,17 @@
+package pers.hong.project.common;
+
+/**
+ * @Description: 常量设置
+ * @Auther: zwh
+ * @Date: 2018/09/18
+ */
+public class Constants {
+    public static final int RESULT_CODE_SUCCESS = 200;  // 成功处理请求
+    public static final int RESULT_CODE_BAD_REQUEST = 412;  // 请求错误
+    public static final int RESULT_CODE_NOT_LOGIN = 402;  // 未登录
+    public static final int RESULT_CODE_PARAM_ERROR = 406;  // 传参错误
+    public static final int RESULT_CODE_SERVER_ERROR = 500;  // 服务器错误
+
+    public final static int PAGE_SIZE = 10;//默认分页条数
+
+}
---
+++ src/main/java/pers/hong/project/common/Result.java
@@ -0,0 +1,60 @@
+package pers.hong.project.common;
+
+import java.io.Serializable;
+
+/**
+ * @Description: API响应结果封装类
+ * @Auther: zwh
+ * @Date: 2018/09/18
+ */
+public class Result<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int resultCode;
+    private String message;
+    private T data;
+
+    public Result() {
+    }
+
+    public Result(int resultCode, String message) {
+        this.resultCode = resultCode;
+        this.message = message;
+    }
+
+    public int getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(int resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Result failure(String code) {
+        return new Result(500, "服务错误");
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "resultCode=" + resultCode +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
